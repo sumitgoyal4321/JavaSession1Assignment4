11347a4019c9c3ad2e30c4859d1b908fd67bd9c9
sumitgoyal4321 <sumitgoyal4321@gmail.com>
2016-03-03 22:53:29 +0530

    Added files via upload

---
+++ SolvingExpressions.java
@@ -0,0 +1,14 @@
+
+public class SolvingExpressions {
+	public static void main(String[]str)
+	{
+		int x = 5;
+		int y = 10;
+		
+		System.out.println(x+y*2);
+		System.out.println(x-y*2);
+		System.out.println((x+y)*2);
+		System.out.println(y%x);
+	}
+
+}
